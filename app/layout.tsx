16914a41077af254e21efd9e25eb6d9b1264d6d5
Abdee Alfarizi <112331180+alfarizi25@users.noreply.github.com>
2025-03-10 13:41:37 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo1.png" sizes="any" />
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
